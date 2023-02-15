0a410c2b24a4e2a1dd8e6422742489d39bd15ba6
unknown <matheus.ribaas@hotmail.com>
2023-02-15 20:25:54 -0300

    primeiro commit

---
+++ cypress/Integration/login.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />  
+
+context('Funcionalidade Login', () =>{
+
+    it('Deve fazer login com sucesso', () => {
+
+    })
+
+    it('deve exibir uma mensagem de erro ao inserir usuario ou senha invaldo', () => {
+
+        
+    })
+})
